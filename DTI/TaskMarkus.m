e2b4afd33283a61e1e3e1b429d24789049beb0ab
arefks <arefks@gmail.com>
2023-06-13 17:28:22 +0200

    Creating PrismFile: FiberCountCompare2-Comparing multiple left and right regions. Lukas volz presentation

--- DTI/TaskMarkus.m
+++ DTI/TaskMarkus.m
@@ -7,11 +7,11 @@
 
 %% Specification
 
-filename = "Y:\Desktop\Data\CRC_WP1\inputs\mri\proc_data\Results\AIDAconnect\OutputDTI_thres0_proc2"; % path to a processed MAT-File
+filename = "Z:\CRC_WP1\outputs\AIDAconnet_results\OutputDTI_thres0"; % path to a processed MAT-File
 files = dir(fullfile(filename,"**","*.mat"));
 
-ROI_L = ["L MOp", "L MOs", "L SSp-ul","L SSp-ll", "L SSs"];
-ROI_R = ["R MOp", "R MOs", "R SSp-ul","R SSp-ll", "R SSs"];
+ROI_L = ["L MOp", "L MOs", "L SSp-un","L SSp-ul","L SSp-n","L SSp-m","L SSp-ll","L SSp-bfd"];
+ROI_R =  ["R MOp", "R MOs", "R SSp-un","R SSp-ul","R SSp-n","R SSp-m","R SSp-ll","R SSp-bfd"];
 days = ["Baseline","P1","P7","P14","P21","P28"];
 group = ["StrokeGood", "StrokeBad", "Sham"];
 %% Do not modify the following lines
@@ -55,49 +55,52 @@ for cc = 1:size(Final_Data,4)
 Comparisons{cc} = [Final_Data(:,:,1,cc),Final_Data(:,:,2,cc),Final_Data(:,:,3,cc)]; 
 end
 
+temp = sum(Final_Data,4);
+Prism_mat = [temp(:,:,1),temp(:,:,2),temp(:,:,3)];
+
 %% Box plots
-% Define the parameters for the plot
-plotColors = ['r', 'g', 'b']; % colors for each group
-boxWidth = 0.6; % width of the box
-boxLineWidth = 1; % width of the box border
-fontSize = 12; % font size of the labels and title
-labelFontSize = 10; % font size of the axis labels
-titleFontSize = 14; % font size of the plot title
-
-% Loop over each region combination
-for i = 1:size(ROI_L, 2)
-    for j = 1:size(ROI_R, 2)
-        
-        % Create a new figure for each region combination
-        figure('Name', [ROI_L{i} ' ' ROI_R{j}]);
-        
-        % Loop over each timepoint
-        for k = 1:size(days, 2)
-            
-            % Get the data for each group
-            data = [];
-            for gg = 1:size(group, 2)
-                data_g = Final_Data(k, :, gg, j, i);
-                data = [data; data_g];
-            end
-            
-            % Create a subplot for each timepoint
-            subplot(2, 3, k);
-            
-            % Create the boxplot for each group
-            boxplot(data, 'Colors', plotColors, 'Widths', boxWidth, 'Symbol', '+', 'Whisker', 1.5, 'LineWidth', boxLineWidth);
-            
-            % Set the axis labels and title
-            xlabel('Group', 'FontSize', labelFontSize);
-            ylabel('Fiber Count', 'FontSize', labelFontSize);
-            title([days{k} ' - ' ROI_L{i} ' ' ROI_R{j}], 'FontSize', titleFontSize);
-            set(gca, 'XTickLabel', group, 'FontSize', fontSize);
-            
-        end
-        
-        % Adjust the position of the subplots
-        set(gcf, 'Position', get(0, 'Screensize'));
-        
-    end
-end
-
+% % Define the parameters for the plot
+% plotColors = ['r', 'g', 'b']; % colors for each group
+% boxWidth = 0.6; % width of the box
+% boxLineWidth = 1; % width of the box border
+% fontSize = 12; % font size of the labels and title
+% labelFontSize = 10; % font size of the axis labels
+% titleFontSize = 14; % font size of the plot title
+% 
+% % Loop over each region combination
+% for i = 1:size(ROI_L, 2)
+%     for j = 1:size(ROI_R, 2)
+%         
+%         % Create a new figure for each region combination
+%         figure('Name', [ROI_L{i} ' ' ROI_R{j}]);
+%         
+%         % Loop over each timepoint
+%         for k = 1:size(days, 2)
+%             
+%             % Get the data for each group
+%             data = [];
+%             for gg = 1:size(group, 2)
+%                 data_g = Final_Data(k, :, gg, j, i);
+%                 data = [data; data_g];
+%             end
+%             
+%             % Create a subplot for each timepoint
+%             subplot(2, 3, k);
+%             
+%             % Create the boxplot for each group
+%             boxplot(data, 'Colors', plotColors, 'Widths', boxWidth, 'Symbol', '+', 'Whisker', 1.5, 'LineWidth', boxLineWidth);
+%             
+%             % Set the axis labels and title
+%             xlabel('Group', 'FontSize', labelFontSize);
+%             ylabel('Fiber Count', 'FontSize', labelFontSize);
+%             title([days{k} ' - ' ROI_L{i} ' ' ROI_R{j}], 'FontSize', titleFontSize);
+%             set(gca, 'XTickLabel', group, 'FontSize', fontSize);
+%             
+%         end
+%         
+%         % Adjust the position of the subplots
+%         set(gcf, 'Position', get(0, 'Screensize'));
+%         
+%     end
+% end
+% 
